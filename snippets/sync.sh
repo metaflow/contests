50da7beb1929f415dd871df848638d8ae97a813a
Mikhail Goncharov <mgoncharov@setronica.com>
2015-06-02 10:18:15 +0600

    fix sync

--- snippets/sync.sh
+++ snippets/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -uv ./cp*.sublime-snippet /home/meta/.config/sublime-text-3/Packages/User/
-rsync -uv /home/meta/.config/sublime-text-3/Packages/User/cp*.sublime-snippet .
+rsync -uv ./cp*.sublime-snippet $HOME/.config/sublime-text-3/Packages/User/
+rsync -uv $HOME/.config/sublime-text-3/Packages/User/cp*.sublime-snippet .
