baa5a7a415790dc7a9fd69c4df13d0c4b3bde1c0
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2015-06-13 11:13:16 +0600

    551/d AC, fibonacci snippet

--- codeforces/551/D.cc
+++ codeforces/551/D.cc
@@ -15,27 +15,28 @@ using vll = vector<ll>;
 const int INF = numeric_limits<int>::max();
 const double EPS = 1e-10;
 
-vll m_pow(vll &a, ll n, ll m) {
+vll __fibonacci_m_pow(vll &a, ll n, ll mod) {
   if (n == 1) return a;
   vll r(4), m1, m2;
   if (n % 2 == 0) {
-    m1 = m2 = m_pow(a, n / 2, m);
+    m1 = m2 = __fibonacci_m_pow(a, n / 2, mod);
   } else {
     m1 = a;
-    m2 = m_pow(a, n - 1, m);
+    m2 = __fibonacci_m_pow(a, n - 1, mod);
   }
-  r[0] = (m1[0] * m2[0] + m1[1] * m2[2]) % m;
-  r[1] = (m1[0] * m2[1] + m1[1] * m2[3]) % m;
-  r[2] = (m1[2] * m2[0] + m1[3] * m2[2]) % m;
-  r[3] = (m1[2] * m2[1] + m1[3] * m2[3]) % m;
+  r[0] = (m1[0] * m2[0] + m1[1] * m2[2]) % mod;
+  r[1] = (m1[0] * m2[1] + m1[1] * m2[3]) % mod;
+  r[2] = (m1[2] * m2[0] + m1[3] * m2[2]) % mod;
+  r[3] = (m1[2] * m2[1] + m1[3] * m2[3]) % mod;
   return r;
 }
 
-ll fib(ll n, ll m) {
+// n >= 1, {1, 1, 2, 3, 5, ...}
+ll fibonacci(ll n, ll mod) {
   vll a(4);
   a[0] = 1; a[1] = 1; a[2] = 1; a[3] = 0;
-  auto r = m_pow(a, n, m);
-  return r[0];
+  auto r = __fibonacci_m_pow(a, n, mod);
+  return r[1];
 }
 
 ll pow2(ll n, ll m) {
@@ -49,25 +50,25 @@ ll pow2(ll n, ll m) {
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  ll n, k, l, m;
-  // cout << fib(1, 100) << endl;
-  // cout << fib(2, 100) << endl;
-  // cout << fib(3, 100) << endl;
-  // cout << fib(4, 100) << endl;
+  llu n, k, l, m;
   while (cin >> n >> k >> l >> m) {
-     ll zeroes = fib(n + 1, m);
-     ll ones = (pow2(n, m) - zeroes + m) % m;
-     // cerr << zeroes << " 0" << endl;
-     // cerr << ones << " 1" << endl;
-     ll r = 1;
-     for (ll i = 0; i < l; i++) {
-       if (k & (1LL << i)) {
-          r = (r * ones) % m;
-       } else {
-          r = (r * zeroes) % m;
-       }
+    if (l < 64 && k >= (1ULL << l)) {
+      cout << 0 << endl;
+      continue;
+    }
+    ll zeroes = fibonacci(n + 2, m);
+    ll ones = (pow2(n, m) - zeroes + m) % m;
+    // cerr << zeroes << " 0" << endl;
+    // cerr << ones << " 1" << endl;
+    ll r = 1;
+    for (llu i = 0; i < l; i++) {
+     if (k & (1LL << i)) {
+        r = (r * ones) % m;
+     } else {
+        r = (r * zeroes) % m;
      }
-     cout << r << endl;
+    }
+    cout << (r % m) << endl;
   }
 }
 
