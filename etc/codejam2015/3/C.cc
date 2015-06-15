d50f6b13fa4c97f526fb71c30329834508b80b86
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2015-06-15 21:04:01 +0600

    round 3 C WA

--- etc/codejam2015/3/C.cc
+++ etc/codejam2015/3/C.cc
@@ -12,54 +12,91 @@ using vvb = vector<vb>;
 using vd = vector<double>;
 using vvd = vector<vd>;
 using vll = vector<ll>;
+using dd = pair<double, double>;
 const int INF = numeric_limits<int>::max();
 const double EPS = 1e-10;
 
-// IDA* TSP with bitmask up to 64 vertices
-ll ida_updated_bound;
-ll estimate(int root, llu visited, vvi& distances) {
-  ll e = 0;
-  for (size_t i = 0; i < distances.size(); i++) {
-    if (visited & (1ull << i)) continue;
-    if (distances[root][i] > e) e = distances[root][i];
+struct quali {
+  ll start, speed;
+  double at(double t) {
+    return start + speed * t;
   }
-  return e;
+  quali(ll a, ll b): start(a), speed(b) {}
+  bool operator < (const quali& rhs) const {
+    if (speed != rhs.speed) return speed < rhs.speed;
+    return start < rhs.start;
+  }
+};
+
+// time, position
+stack<pair<int, double>> get_hull(vector<quali>& q) {
+  stack<pair<int, double>> s; // index, time
+  for (size_t i = 0; i < q.size(); i++) {
+    auto u = q[i];
+    while (!s.empty()) {
+      int j; double t;
+      tie(j, t) = s.top();
+      if (q[j].at(t) > u.at(t)) break;
+      s.pop();
+    }
+    if (s.empty()) {
+      s.emplace(i, 0);
+    } else {
+      auto v = q[s.top().first];
+      if (v.speed == u.speed) continue; // same quali
+      double t = double(u.start - v.start) / double(v.speed - u.speed);
+      s.emplace(i, t);
+    }
+    // cout << "T " << s.top().first << " " << s.top().second << endl;
+  }
+  return s;
 }
 
-bool tsp_dfs(int position, llu visited, ll traveled,
-             ll bound, vvi& distances) {
-  ll e = estimate(position, visited, distances) + traveled;
-  if (e > bound) {
-    ida_updated_bound = min(ida_updated_bound, e);
-    return false;
+double intersect(stack<pair<int, double>> s, vector<quali>& q, quali me) {
+  while (!s.empty()) {
+    int j; double t;
+    tie(j, t) = s.top();
+    if (q[j].at(t) > me.at(t)) break;
+    s.pop();
   }
-  if (visited + 1 == (1ull << (distances.size()))) return true;
-  for (size_t i = 0; i < distances.size(); i++) {
-    if (visited & (1ull << i)) continue;
-    if (tsp_dfs(i, visited | (1ull << i), traveled + distances[position][i],
-                bound, distances)) return true;
-  }
-  return false;
+  if (s.empty()) return 0;
+  auto v = q[s.top().first];
+  double t = double(me.start - v.start) / double(v.speed - me.speed);
+  return t;
 }
-
-ll tsp(vvi& distances) {
-  ll bound = estimate(0, 1, distances);
-  while (bound != INF) {
-    ida_updated_bound = INF;
-    if (tsp_dfs(0, 1, 0, bound, distances)) break;
-    bound = ida_updated_bound;
-  }
-  return bound;
-}
-
-
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
   int tcc; cin >> tcc;
   for (int tc = 1; tc <= tcc; tc++) {
-
-    ll answer = 0;
-    cout << "Case #" << tc << ": " << answer << endl;
+    ll Y, N;
+    cin >> Y >> N;
+    vector<quali> R, L;
+    vll pp(N), ss(N);
+    for (ll i = 0; i < N; i++) cin >> pp[i];
+    for (ll i = 0; i < N; i++) cin >> ss[i];
+    for (ll i = 0; i < N; i++) {
+      if (pp[i] > 0) {
+        R.emplace_back(pp[i], ss[i]);
+      } else {
+        L.emplace_back(-pp[i], ss[i]);
+      }
+    }
+    sort(R.begin(), R.end());
+    sort(L.begin(), L.end());
+    auto hr = get_hull(R);
+    auto hl = get_hull(L);
+    quali me(0, Y);
+    // R -> L
+    double t = intersect(hr, R, me);
+    me.start = -t * 2 * Y;
+    double best = max(t, intersect(hl, L, me));
+    // L -> R
+    me.start = 0;
+    t = intersect(hl, L, me);
+    me.start = - t * 2 * Y;
+    best = min(best, max(t, intersect(hr, R, me)));
+    cout << "Case #" << tc << ": "
+    << fixed << setprecision(7) << best << endl;
   }
 }
