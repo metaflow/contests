3f5987e88b485e1893960916c1d2b8e235a2ab25
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2015-09-04 12:18:55 +0600

    665 300 AC

--- topcoder/665/LuckySum.cc
+++ topcoder/665/LuckySum.cc
@@ -17,111 +17,89 @@ const double EPS = 1e-10;
 
 class LuckySum {
     public:
-    long long construct(string note) {
-      ll n = note.size();
-      // 3 - any, 1 - produces, 2 - not produces, 0 - impossible
+    bool check(vi& v, string s, int pos) {
+      if (s[pos] == '?') return v[pos] > 0;
+      return v[pos] == (s[pos] - '0');
+    }
+
+    vi f(vi v, string s, int fa, int fb, int pos) {
+      if (pos < s.size()) {
+        vi va;
+        if (pos >= fa) {
+          va.emplace_back(4);
+          va.emplace_back(7);
+        } else {
+          va.emplace_back(0);
+        }
+        vi vb;
+        if (pos >= fb) {
+          vb.emplace_back(4);
+          vb.emplace_back(7);
+        } else {
+          vb.emplace_back(0);
+        }
+        for (auto a : va) {
+          for (auto b : vb) {
+            vi c = v;
+            c[pos] += a + b;
+            c[pos - 1] += c[pos] / 10;
+            c[pos] %= 10;
+            if (check(c, s, pos - 1)) {
+              c = f(c, s, fa, fb, pos + 1);
+              if (c[0] == 0) return c;
+            }
+          }
+        }
+      } else {
+        if (check(v, s, pos - 1)) return v;
+      }
+      v[0] = 1;
+      return v;
+    }
+
+    ll lucky(string s) {
+      ll n = s.size();
       vi carry(n, 3);
       carry[0] = 2;
       bool ok = true;
-      switch (note[0]) {
-        case '?': break;
-        case '1':
-          if (n == 1) { ok = false; break; }
-          carry[1] &= 1;
-          break;
-        case '4':
-          if (n == 1) { ok = false; break; }
-          carry[1] &= 2;
-          break;
-        case '5':
-          if (n == 1) { ok = false; break; }
-          carry[1] &= 1;
-          break;
-        case '7':
-          if (n == 1) { ok = false; break; }
-          carry[1] &= 2;
-          break;
-        case '8': break;
-        case '9':
-          if (n == 1) { ok = false; break; }
-          carry[1] &= 1;
-          break;
-        case default:
-          ok = false;
-          break;
-      }
-      for (ll i = 1; i < n; i++) {
-        switch (note[i]) {
-          case '1':
-            carry[i] &= 1;
-            if (i + 1 == n) continue;
-            carry[i + 1] &= 2;
-            break;
-          case '2':
-            if (i + 1 == n) {ok = false; break; }
-            carry[i + 1] &= 1;
-            carry[i] &= 1;
-            break;
-          case '4':
-            if (i + 1 == n) continue;
-            carry[i + 1] &= 2;
-            break;
-          case '5':
-            if (i + 1 == n) { ok = false; break; }
-            carry[i + 1] &= 1;
-            break;
-          case '7':
-            if (i + 1 == n) { ok = false; break; }
-            carry[i + 1] &= 2;
-            break;
-          case '8': break;
-          case '9': break;
-            if (i + 1 == n) { ok = false; break; }
-            carry[i + 1] &= 1;
+      for (ll i = 0; i < n; i++) {
+        bool last = (i == n - 1);
+        switch (s[i]) {
           case '?': break;
-          default:
-            ok = false;
-            break;
+          case '1': if (last || i != 0) carry[i] &= 1; break;
+          case '2': if (last) { ok = false; }
+                    else { carry[i + 1] &= 1; carry[i] &= 1; } break;
+          case '4': if (last) { carry[i] &= 1; } else { carry[i + 1] &= 2; } break;
+          case '5': if (last) { ok = false; } else { carry[i + 1] &= 1; } break;
+          case '7': if (last) { ok = false; } else { carry[i + 1] &= 2; carry[i] &= 2; } break;
+          case '8': carry[i] &= 2; break;
+          case '9': if (last) { ok = false; } else { carry[i + 1] &= 1; carry[i] &= 2; } break;
+          default: ok = false; break;
         }
       }
-      for (ll i = 0; i < n; i++) if (carry[i] == 0) ok = false;
-      vi solution(n, 9);
-      for (ll l = 1; l <= n; l++) {
-        vi c = carry;
-        vi candidate(n);
-        bool g = true;
-        if (note[0] == '?') {
-          if (n == 1) {
-            candidate[0] = 8;
-          } else {
-            if (l == n) {
-              if (c[1] & 2) {
-                candidate[0] = 8;
-                c[1] = 2;
-              } else {
-                candidate[0] = 9;
+      for (auto i : carry) ok = ok && (i != 0);
+      ll best = - 1;
+      if (ok) {
+        s = "0" + s;
+        for (ll i = 1; i <= 2; i++) {
+          for (ll j = i; j <= n; j++) {
+            vi v(n + 1, 0);
+            v = f(v, s, i, j, 1);
+            if (v[0] != 1) {
+              ll a = 0;
+              for (auto k : v) {
+                a = a * 10 + k;
               }
-            } else {
-              if (c[1] & 1) {
-                candidate[0] = 1;
-                c[1] = 1;
-              } else {
-                candidate[0] = 4;
-              }
+              if (a < best || best == -1) best = a;
             }
           }
-        } else {
-          candidate[0] = note[0] - '0';
-        }
-        for (ll i = 1; i < n; i++) {
-          bool b = (i >= n - l);
-          switch (note[i]) {
-            case '1':
-
-          }
         }
       }
-      return 0;
+      return best;
+    }
+
+    long long construct(string note) {
+      return lucky(note);
     }
 
 // BEGIN CUT HERE
@@ -129,7 +107,7 @@ class LuckySum {
 	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
 	private:
 	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
-	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void verify_case(int Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "1FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
 	void test_case_0() { string Arg0 = "?"; long long Arg1 = 8LL; verify_case(0, Arg1, construct(Arg0)); }
 	void test_case_1() { string Arg0 = "?1"; long long Arg1 = 11LL; verify_case(1, Arg1, construct(Arg0)); }
 	void test_case_2() { string Arg0 = "4?8"; long long Arg1 = 448LL; verify_case(2, Arg1, construct(Arg0)); }
@@ -146,3 +124,4 @@ int main() {
     ___test.run_test(-1);
 }
 // END CUT HERE
+
