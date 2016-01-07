9b31a13484db5865f43dfabef55eceb2168dd80f
Mikhail Goncharov <metaflow@yandex-team.ru>
2016-01-07 15:48:36 +0600

    another version

--- codeforces/582/B.cc
+++ codeforces/582/B.cc
@@ -11,52 +11,74 @@ using vd = vector<double>; using vvd = vector<vd>;
 const int INF = numeric_limits<int>::max();
 const double EPS = 1e-10;
 const l e5 = 100000, e6 = 1000000, e7 = 10000000, e9 = 1000000000;
-
+const l MAX = 301;
 vl as;
-l n;
 
-vl lis_frames(function<bool(l)> p) {
+l lis(function<bool(l)> p) {
   vl seq;
-  vl per_frame;
-  per_frame.push_back(0);
-  for (l k = 0; k < n; k++) {
-    for (l i = 0; i < n; i++) {
-      auto x = as[i];
-      if (!p(x)) continue;
-      auto q = upper_bound(seq.begin(), seq.end(), x);
-      if (q == seq.end()) {
-        seq.push_back(x);
-      } else {
-        *q = x;
-      }
+  for (auto a : as) {
+    if (!p(a)) continue;
+    auto i = upper_bound(seq.begin(), seq.end(), a);
+    if (i == seq.end()) {
+      seq.push_back(a);
+    } else {
+      *i = a;
     }
-    per_frame.push_back(seq.size());
   }
-  return per_frame;
+  return seq.size();
 }
 
-const l MAX = 500;
+vvl mmax(vvl a, vvl b) {
+  l n = a.size();
+  vvl c(n, vl(n));
+  for (l i = 0; i < n; i++) {
+    for (l j = 0; j < n; j++) {
+      for (l k = 0; k < n; k++) {
+        if (as[k] < as[i] || as[k] > as[j]) continue;
+        c[i][j] = max(c[i][j], a[i][k] + b[k][j]);
+      }
+    }
+  }
+  return c;
+}
+
+vvl pow_max(vvl a, l t) {
+  l n = a.size();
+  vvl r;
+  bool identity = true;
+  while (t) {
+    if (t % 2) {
+      if (identity) {
+        identity = false;
+        r = a;
+      } else {
+        r = mmax(r, a);
+      }
+    }
+    a = mmax(a, a);
+    t /= 2;
+  }
+  return r;
+}
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  while (cin >> n) {
-    l t; cin >> t;
+  l n, t;
+  while (cin >> n >> t) {
     as.resize(n);
-    vl counts(MAX);
     for (l i = 0; i < n; i++) cin >> as[i];
-    for (auto a : as) counts[a]++;
-    vb checked(MAX);
+    vvl m(n, vl(n));
+    for (l i = 0; i < n; i++) {
+      for (l j = 0; j < n; j++) {
+        l a = as[i], b = as[j];
+        m[i][j] = lis([&a,&b](const l& x) {return x >= a && x <= b; });
+      }
+    }
+    m = pow_max(m, t);
     l best = 0;
-    for (int i = 0; i < n; i++) {
-      auto x = as[i];
-      if (checked[x]) continue;
-      checked[x] = true;
-      vl lis_start = lis_frames(bind2nd(less_equal<l>(), x));
-      vl lis_end =   lis_frames(bind2nd(greater_equal<l>(), x));
-      for (l a = 0; a <= min(n, t); a++) {
-        for (l b = 0; b <= min(t - a, n); b++) {
-          best = max(best, lis_start[a] + counts[x] * (t - a - b) + lis_end[b]);
-        }
+    for (l i = 0; i < n; i++) {
+      for (l j = 0; j < n; j++) {
+        best = max(best, m[i][j]);
       }
     }
     cout << best << endl;
