56ca7a8e11196c65d019e6f77a83a534c320eedf
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2016-05-01 22:13:15 +0600

    50 AC

--- hackerrank/euler/50.cc
+++ hackerrank/euler/50.cc
@@ -35,52 +35,29 @@ bool is_prime(vl& primes, l n) {
   return true;
 }
 
-chrono::high_resolution_clock::duration prime_time;
-
-l get_seq_at(vl& seq, size_t at, l n, vl& primes) {
-  auto t1 = chrono::high_resolution_clock::now();
-  while (seq.size() <= at) {
-    l x = seq.empty() ? n : seq.back() - 1;
-    if (x % 2 == 0 && x != 2) x--;
-    while (!is_prime(primes, x)) x -= 2;
-    assert(x > 1);
-    seq.push_back(x);
-  }
-  prime_time += chrono::high_resolution_clock::now() - t1;
-  return seq[at];
-}
-
 ll solve(l n, vl& primes, vl& sum) {
   vl seq;
   l best = 0;
   l best_x = 0;
   l right = 0;
   l size = sum.size();
-  size_t best_i = 0;
   for (l left = 0; left < size; left++) {
-    // cerr << left << endl;
-    size_t i = 0;
-    l x = get_seq_at(seq, i, n, primes);
-    while (sum[right] - sum[left] < x) right++;
+    while (sum[right] - sum[left] < n) right++;
     if (right - left <= best) break;
+    while (sum[right] - sum[left] > n || (right > 1 && (sum[right] - sum[left]) % 2 == 0)) right--;
     while (true) {
-      while (sum[right] - sum[left] > x) right--;
       assert(right > left);
       if (right - left <= best) break;
-      if (sum[right] - sum[left] == x) {
+      if (is_prime(primes, sum[right] - sum[left])) {
         if (right - left > best) {
           best = right - left;
-          best_x = x;
-          best_i = i;
+          best_x = sum[right] - sum[left];
         }
         break;
       }
-      i++;
-      x = get_seq_at(seq, i, n, primes);
+      if (right > 2) { right -= 2; } else { right--; }
     }
   }
-  cout << "best i " << best_i << endl;
-  // cout << seq.size() << endl;
   return make_pair(best_x, best);
 }
 
@@ -91,15 +68,8 @@ int main() {
   for (size_t i = 1; i < sum.size(); i++) sum[i] = sum[i - 1] + primes[i - 1];
   l T; cin >> T;
   while (T--) {
-    cout << "---" << endl;
-    prime_time = chrono::high_resolution_clock::duration::zero();
     l n; cin >> n;
-    auto t1 = chrono::high_resolution_clock::now();
     auto s = solve(n, primes, sum);
-    auto t2 = chrono::high_resolution_clock::now();
-    cout << "total " << chrono::duration_cast<chrono::microseconds>(t2 - t1).count() << endl;
-    cout << "prime " << chrono::duration_cast<chrono::microseconds>(prime_time).count() << endl;
     cout << s.first << " " << s.second << endl;
-    cout << (n - s.first) << endl;
   }
 }
