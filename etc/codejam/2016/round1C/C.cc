e0e7d4a53667dfcbfe871862ba74f782d856ca25
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2016-05-13 15:27:37 +0600

    codejam round 1c

---
+++ etc/codejam/2016/round1C/C.cc
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>; using vvi = vector<vi>;
+using ii = pair<int,int>; using vii = vector<ii>;
+using l = long long; using vl = vector<l>; using vvl = vector<vl>; using vvvl = vector<vvl>;
+using ll = pair<l,l>; using vll = vector<ll>; using vvll = vector<vll>;
+using lu = unsigned long long;
+using vb = vector<bool>; using vvb = vector<vb>;
+using vd = vector<double>; using vvd = vector<vd>;
+const int INF = numeric_limits<int>::max();
+const double EPS = 1e-10;
+const l e5 = 100000, e6 = 1000000, e7 = 10000000, e9 = 1000000000;
+
+l gJ, gP, gS, gK;
+vvvl best;
+l best_days;
+
+void backtrack(l j, l p, l s, vvvl& used, vvl& jp, vvl& js, vvl& ps, l days) {
+  if (s == gS) { p++; s = 0; }
+  if (p == gP) { j++; p = 0; }
+  if (j == gJ) {
+    if (days <= best_days) return;
+    best_days = days;
+    best = used;
+    return;
+  }
+  if ((jp[j][p] < gK) && (js[j][s] < gK) && (ps[p][s] < gK)) {
+    used[j][p][s] = 1;
+    jp[j][p]++;
+    js[j][s]++;
+    ps[p][s]++;
+    backtrack(j, p, s + 1, used, jp, js, ps, days + 1);
+    used[j][p][s] = 0;
+    jp[j][p]--;
+    js[j][s]--;
+    ps[p][s]--;
+  }
+  backtrack(j, p, s + 1, used, jp, js, ps, days);
+}
+
+vector<tuple<l,l,l>> solve_brute(l J, l P, l S, l K) {
+  best_days = 0;
+  gJ = J; gP = P; gS = S; gK = K;
+  vvvl m(J, vvl(P, vl(S, 0)));
+  vvl jp(J, vl(P));
+  vvl js(J, vl(S));
+  vvl ps(P, vl(S));
+  backtrack(0, 0, 0, m, jp, js, ps, 0);
+  vector<tuple<l,l,l>> solution;
+  for (l j = 0; j < J; j++)
+  for (l p = 0; p < P; p++)
+  for (l s = 0; s < S; s++) {
+    if (best[j][p][s]) {
+      solution.emplace_back(j + 1, p + 1, s + 1);
+    }
+  }
+  return solution;
+}
+
+vector<tuple<l,l,l>> solve(l J, l P, l S, l K) {
+  vvl jp(J, vl(P));
+  vvl ps(P, vl(S));
+  vvl js(J, vl(S));
+  vector<tuple<l,l,l>> solution;
+  for (l j = 0; j < J; j++)
+  for (l p = 0; p < P; p++) {
+    for (l s = 0; s < S && jp[j][p] < K; s++) {
+      if (ps[p][s] < K && js[j][s] < K) {
+        jp[j][p]++;
+        ps[p][s]++;
+        js[j][s]++;
+        solution.emplace_back(j + 1, p + 1, s + 1);
+      }
+    }
+  }
+  return solution;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  for (l J = 1; J <= 3; J++)
+  for (l P = J; P <= 3; P++)
+  for (l S = P; S <= 3; S++)
+  for (l K = 1; K <= 10; K++) {
+    auto a = solve_brute(J, P, S, K);
+    auto b = solve(J, P, S, K);
+    if (a.size() != b.size()) {
+      cout << J << " " << P << " " << S << " " << K << endl;
+      cout << a.size() << " != " << b.size() << endl;
+      return 0;
+    }
+  }
+  cout << "OK" << endl;
+  return 0;
+  /*l tcc; cin >> tcc;
+  for (l tc = 1; tc <= tcc; tc++) {
+    cin >> J >> P >> S >> K;
+    cout << "Case #" << tc << ": ";
+    auto solution = solve(J, P, S, K);
+  }*/
+}
