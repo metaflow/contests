3510773d71fc9881690e9abf83cd2b10829b8602
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2017-04-10 16:51:52 +0100

    updates

---
+++ codeforces/69/C.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector<int>; using vvi = vector<vi>;
+using ii = pair<int,int>; using vii = vector<ii>;
+using l = long long; using vl = vector<l>; using vvl = vector<vl>;
+using ll = pair<l,l>; using vll = vector<ll>; using vvll = vector<vll>;
+using lu = unsigned long long;
+using vb = vector<bool>; using vvb = vector<vb>;
+using vd = vector<double>; using vvd = vector<vd>;
+const int INF = numeric_limits<int>::max();
+const double EPS = 1e-10; static constexpr auto PI = acos(-1);
+const l e0=1, e3=1000, e5=100000, e6=10*e5, e7=10*e6, e8=10*e7, e9=10*e8;
+#define ALL(x) begin(x), end(x)
+#define F(a,b,c) for (l a = l(b); a < l(c); a++)
+#define B(a,b,c) for (l a = l(b); a > l(c); a--)
+
+// #define ONLINE_JUDGE
+#if defined ONLINE_JUDGE
+const bool enable_log = false;
+#else
+const bool enable_log = true;
+#endif
+struct VoidStream { void operator&(std::ostream&) { } };
+#define LOG !(enable_log) ? (void) 0 : VoidStream() & cerr
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  l friendCount, basicCount, compositeCount, purchases;
+  while (cin >> friendCount >> basicCount >> compositeCount >> purchases) {
+    map<string, l> names;
+    F(i, 0, basicCount) {
+      string s; cin >> s;
+      // LOG << s << endl;
+      l j = names.size();
+      names[s] = j;
+    }
+    // for (auto n : names) LOG << n.first << ' ' << n.second << endl;
+    string s;
+    getline(cin, s);
+    vvl composite(101, vl(basicCount));
+    F(i, 0, compositeCount) {
+      getline(cin, s);
+      // LOG << s << endl;
+      stringstream ss(s);
+      ss >> s;
+      s.pop_back();
+      l c = names.size();
+      names[s] = c;
+      while (ss >> s) {
+        l j = names[s];
+        string no; ss >> no;
+        if (no.back() == ',') no.pop_back();
+        composite[c][j] += stol(no);
+      }
+    }
+    vvl arts(friendCount, vl(names.size()));
+    F(i, 0, purchases) {
+      l f; cin >> f >> s; f--;
+      l a = names[s];
+      arts[f][a]++;
+      F(k, basicCount, basicCount + compositeCount) {
+        bool ok = true;
+        F(m, 0, basicCount) ok = ok and (composite[k][m] <= arts[f][m]);
+        if (ok) {
+          arts[f][k]++;
+          F(m, 0, basicCount) arts[f][m] -= composite[k][m];
+          break;
+        }
+      }
+    }
+    F(i, 0, friendCount) {
+      l n = 0;
+      F(j, 0, names.size()) if (arts[i][j]) n++;
+      cout << n << '\n';
+      for (auto item : names) {
+        if (arts[i][item.second]) cout << item.first << ' ' << arts[i][item.second] << '\n';
+      }
+    }
+  }
+}
--- hackerrank/hiringSprint/lsr.cc
+++ hackerrank/hiringSprint/lsr.cc
@@ -1,37 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-using vi = vector<int>;
-using ii = pair<int,int>;
-using vii = vector<ii>;
-using vvi = vector<vi>;
-using ll = long long;
-using llu = unsigned long long;
-using vb = vector<bool>;
-using vvb = vector<vb>;
-using vd = vector<double>;
-using vvd = vector<vd>;
-using vll = vector<ll>;
+using vi = vector<int>; using vvi = vector<vi>;
+using ii = pair<int,int>; using vii = vector<ii>;
+using l = long long; using vl = vector<l>; using vvl = vector<vl>;
+using ll = pair<l,l>; using vll = vector<ll>; using vvll = vector<vll>;
+using lu = unsigned long long;
+using vb = vector<bool>; using vvb = vector<vb>;
+using vd = vector<double>; using vvd = vector<vd>;
 const int INF = numeric_limits<int>::max();
 const double EPS = 1e-10;
+const l e0=1, e3=1000, e5=100000, e6=e3*e3, e7=10*e6, e8=10*e7, e9=10*e8;
+#define ALL(x) begin(x), end(x)
+#define F(a,b,c) for (l a = l(b); a < l(c); a++)
+#define B(a,b,c) for (l a = l(b); a > l(c); a--)
 
-void sort_suffix_array(vi& sa, vll& rank, int k) {
-  int n = sa.size();
-  vi counts(max(n, 300));
-  for (int i = 0; i < n; i++) {
-    int p = 0;
+// #define ONLINE_JUDGE
+#if defined ONLINE_JUDGE
+const bool enable_log = false;
+#else
+const bool enable_log = true;
+#endif
+struct VoidStream { void operator&(std::ostream&) { } };
+#define LOG !(enable_log) ? (void) 0 : VoidStream() & cerr
+
+void sort_suffix_array(vl& sa, vl& rank, l k) {
+  l n = sa.size();
+  vl counts(max(n, (l)300));
+  F(i, 0, n) {
+    l p = 0;
     if (i + k < n) p = rank[i + k];
     counts[p]++;
   }
-  int s = 0;
+  l s = 0;
   for (size_t i = 0; i < counts.size(); i++) {
-    int t = counts[i];
+    l t = counts[i];
     counts[i] = s;
     s += t;
   }
-  vi updated_sa(n);
+  vl updated_sa(n);
   for (auto i : sa) {
-    int p = 0;
+    l p = 0;
     if (i + k < n) p = rank[i + k];
     updated_sa[counts[p]] = i;
     counts[p]++;
@@ -39,80 +49,77 @@ void sort_suffix_array(vi& sa, vll& rank, int k) {
   swap(sa, updated_sa);
 }
 
-vi build_suffix_array(string s) {
-  size_t n = s.size();
-  vi sa(n);
-  vll rank(n);
-  for (size_t i = 0; i < n; i++) {
+vl build_suffix_array(string s) {
+  l n = s.size();
+  vl sa(n);
+  vl rank(n);
+  F(i, 0, n) {
     rank[i] = s[i];
     sa[i] = i;
   }
-  for (size_t k = 1; k < n; k = k << 1) {
+  for (l k = 1; k < n; k = k << 1) {
     sort_suffix_array(sa, rank, k);
     sort_suffix_array(sa, rank, 0);
-    vll updated_rank(rank.size());
-    int r = 0;
+    vl updated_rank(rank.size());
+    l r = 0;
     updated_rank[sa[0]] = r;
-    for (size_t i = 1; i < n; i++) {
+    F(i, 1, n) {
       if (rank[sa[i]] != rank[sa[i - 1]] ||
-          rank[sa[i] + k] != rank[sa[i - 1] +k]) r++;
+          rank[sa[i] + k] != rank[sa[i - 1] + k]) r++;
       updated_rank[sa[i]] = r;
     }
     swap(rank, updated_rank);
-    if (rank[sa[n - 1]] == int(n - 1)) break;
+    if (rank[sa[n - 1]] == n - 1) break;
   }
   return sa;
 }
 
-vi build_lcp(string &s, vi &sa) {
-  size_t i;
-  vi lcp(sa.size()), plcp(sa.size());
-
-  vi p(sa.size());
+vl build_lcp(string &s, vl &sa) {
+  l n = sa.size();
+  vl lcp(n), plcp(n);
+  vl p(n);
   p[sa[0]] = -1;
-  for (i = 1; i < sa.size(); ++i) p[sa[i]] = sa[i - 1];
-  int l = 0;
-  for (i = 0; i < sa.size(); ++i) {
+  F(i, 1, n) p[sa[i]] = sa[i - 1];
+  l w = 0;
+  F(i, 0, n) {
     if (p[i] == -1) { plcp[i] = 0; continue; }
-    while (s[i + l] == s[p[i] + l] && i + l + 1 < sa.size()) ++l;
-    plcp[i] = l;
-    if (l) --l;
+    while (s[i + w] == s[p[i] + w] && i + w + 1 < n) w++;
+    plcp[i] = w;
+    if (w) --w;
   }
-  for (i = 0; i < sa.size(); ++i) {
-    lcp[i] = plcp[sa[i]];
-  }
+  F(i, 0, n) lcp[i] = plcp[sa[i]];
   return lcp;
 }
 
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
   string s;
-  getline(cin, s);
-  string r = s;
-  reverse(r.begin(), r.end());
-  int n = s.size();
-  s = s + '!' + r;
-  auto sa = build_suffix_array(s);
-  auto lcp = build_lcp(s, sa);
-  int best = 0;
-  for (size_t i = 0; i < sa.size(); i++) {
-    int v = sa[i];
-    if (v >= n) v = n - 1 - (v - n - 1);
-    // cout << sa[i] << " " << v << " " << lcp[i] <<  " " << s.substr(sa[i]) << endl;
-    int p = INF;
-    for (size_t j = i + 1; j < sa.size(); j++) {
-      p = min(p, lcp[j]);
-      if (p == 0 || p <= best) break;
-      if ((sa[j] < n) == (sa[i] < n)) continue;
-      int u = sa[j];
-      if (u >= n) u = n - 1 - (u - n - 1);
-      if (sa[j] < n) swap(u, v);
-      if (u < v) {
-        best = max(best, p);
-      } else {
-        best = max(best, min(p, (u - v + 1) / 2));
+  while (cin >> s) {
+    string r = s;
+    reverse(r.begin(), r.end());
+    l n = s.size();
+    s = s + '#' + r + '!';
+    auto sa = build_suffix_array(s);
+    auto lcp = build_lcp(s, sa);
+    l best = 0;
+    for (size_t i = 0; i < sa.size(); i++) {
+      l v = sa[i];
+      if (v >= n) v = 2*n - v;
+      l p = INF;
+      for (size_t j = i + 1; j < sa.size(); j++) {
+        p = min(p, lcp[j]);
+        if (p <= best) break;
+        if ((sa[j] < n) == (sa[i] < n)) continue;
+        l u = sa[j];
+        if (u >= n) u = 2*n - u;
+        if (sa[j] < n) swap(u, v);
+        if (u < v) {
+          best = max(best, p);
+        } else {
+          best = max(best, min(p, (u - v + 1) / 2));
+        }
       }
     }
+    cout << best << endl;
   }
-  cout << best << endl;
 }
