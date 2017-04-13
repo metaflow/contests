e2ec1ce44f9c37248415f591e6dcc13844a9eab8
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2017-04-13 20:33:33 +0100

    lf

--- template.cpp
+++ template.cpp
@@ -12,6 +12,7 @@ using vd = vector<double>; using vvd = vector<vd>;
 const int INF = numeric_limits<int>::max();
 const double EPS = 1e-10; static constexpr auto PI = acos(-1);
 const l e0=1, e3=1000, e5=100000, e6=10*e5, e7=10*e6, e8=10*e7, e9=10*e8;
+const char LF = '\n';
 #define ALL(x) begin(x), end(x)
 #define F(a,b,c) for (l a = l(b); a < l(c); a++)
 #define B(a,b,c) for (l a = l(b); a > l(c); a--)
