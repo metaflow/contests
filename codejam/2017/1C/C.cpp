b8ccadbe461b9adcb942839b58cc9a910fc08613
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2017-05-02 16:28:57 +0100

    small 1 AC!

--- codejam/2017/1C/C.cpp
+++ codejam/2017/1C/C.cpp
@@ -30,27 +30,28 @@ struct VoidStream { void operator&(std::ostream&) { } };
 
 const l M = 10000;
 
-double dfs(vvd& dp, vl& p, l at, l k) {
+double dfs(vvd& dp, vd& p, l at, l k) {
   if (at + 1 < k) return 0;
   if (k == 0) return 1;
   auto& r = dp[at][k];
   if (r < 0) {
-    double q = p[at]; q /= M;
-    r = q * dfs(dp, p, at - 1, k - 1);
+    r = p[at] * dfs(dp, p, at - 1, k - 1) +
+      (1 - p[at]) * dfs(dp, p, at - 1, k);
   }
   return r;
 }
 
-double prob(vl& p, l k) {
+double prob(vd& p, l k) {
   l n = p.size();
   vvd dp(n, vd(k + 1, -1));
   return dfs(dp, p, n - 1, k);
 }
 
 struct Case {
-  l n, k, u;
-  vl p;
-  l id, z;
+  l n, k;
+  double u;
+  vd p;
+  l id;
   double answer;
 };
 
@@ -58,20 +59,12 @@ Case solve(Case c) {
   auto& p = c.p;
   auto& n = c.n;
   auto& k = c.k;
-  auto& z = c.z;
   auto& u = c.u;
   sort(all(p));
-  while (u) {
-    p[0]++;
-    u--;
-    F(i, 1, n)
-      if (p[i] < p[i - 1] and u) {
-        p[i]++;
-        u--;
-      } else {
-        break;
-      }
-  }
+  double sum = p[0];
+  l j = 1;
+  while (j < n and ((sum + u) / j > p[j])) { sum += p[j]; j++; }
+  F(i, 0, j) p[i] = (sum + u) / j;
   double v = prob(p, k);
   c.answer = v;
   return c;
@@ -83,25 +76,26 @@ int main() {
   vector<Case> cc(tcc);
   F(i, 0, tcc) {
     l n, k; cin >> n >> k;
-    double x; cin >> x;
-    cc[i].u = x * M;
-    vl p(n);
-    l z = 0;
-    F(j, 0, n) {
-      cin >> x; x *= M;
-      p[j] = x;
-      if (p[j] == 0) z++;
-    }
+    cin >> cc[i].u;
+    vd p(n);
+    F(j, 0, n) cin >> p[j];
     cc[i].p = p;
     cc[i].n = n;
     cc[i].k = k;
     cc[i].id = i;
-    cc[i].z = z;
   }
-  vector<future<Case>> ff;
-  F(i, 0, tcc) ff.emplace_back(async(launch::async, &solve, cc[i]));
   vector<double> answers(tcc);
-  F(i, 0, tcc) answers[i] = ff[i].get().answer;
+  if (1) {
+    F(i, 0, tcc) answers[i] = solve(cc[i]).answer;
+  } else {
+    vector<future<Case>> ff;
+    F(i, 0, tcc) {
+      auto a = async(launch::async, &solve, cc[i]);
+      a.get();
+      ff.emplace_back(move(a));
+    };
+    F(i, 0, tcc) answers[i] = ff[i].get().answer;
+  }
   F(tc, 1, tcc + 1) {
     cout << "Case #" << tc << ": " << fixed << setprecision(9)
          << answers[tc - 1] << lf;
