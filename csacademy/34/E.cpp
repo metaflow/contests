36a9e4f49e0f6450ae1ff41e0089593386b25c77
Mikhail Goncharov <metaflow@yandex-team.ru>
2017-06-22 21:30:17 +0100

    e

--- csacademy/34/E.cpp
+++ csacademy/34/E.cpp
@@ -17,7 +17,7 @@ using vb = vector<bool>; using vvb = vector<vb>;
 using vd = vector<double>; using vvd = vector<vd>;
 using mll = unordered_map<l, l>;
 const l INF = numeric_limits<int>::max();
-const double EPS = 1e-10; static constexpr auto PI = acos(-1);
+const double EPS = 1e-15; static constexpr auto PI = acos(-1);
 const l e0=1, e3=1000, e5=100000, e6=10*e5, e7=10*e6, e8=10*e7, e9=10*e8;
 const char lf = '\n';
 #define all(x) begin(x), end(x)
@@ -34,8 +34,114 @@ bool local = false;
 struct VoidStream { void operator&(std::ostream&) { } };
 #define LOG !(local) ? (void) 0 : VoidStream() & cerr
 
-void solve(istream& cin, ostream& cout) {
+l sign(l n) {
+  if (n < 0) return -1;
+  if (n == 0) return 0;
+  return 1;
+}
 
+// conruent modulo, works for negative
+l cong(l x, l mod) {
+  return (x % mod + mod) % mod;
+}
+
+// (a * b) % mod, safe for l near max
+l mult_mod(l a, l b, l mod) {
+  l x = 0;
+  while (b) {
+    if (b % 2) x = (x + a) % mod;
+    a = (a * 2) % mod;
+    b /= 2;
+  }
+  return x;
+}
+
+// (base^power) % mod, safe for l near max
+l pow_mod(l base, l power, l mod) {
+  l r = 1;
+  while (power) {
+    if (power % 2) r = mult_mod(r, base, mod);
+    base = mult_mod(base, base, mod);
+    power /= 2;
+  }
+  return r;
+}
+
+l divup(l a, l b) { // ceil div
+  return (a + b - 1) / b;
+}
+
+// return gcd(a, b) and set x, y: a * x + b * y = gcd(a, b)
+l extended_euclid(l a, l b, l& x, l& y) {
+  if (b == 0) { x = 1; y = 0; return a; }
+  l d = extended_euclid(b, a % b, x, y);
+  l t = y;
+  y = x - (a / b) * y;
+  x = t;
+  return d;
+}
+
+// return b: a * b = 1 (mod n)
+l inverse_mod(l a, l n) {
+  l x, y;
+  l d = extended_euclid(a, n, x, y);
+  if (d != 1) return 0;
+  return cong(x, n);
+}
+
+// single combintions k from n
+l nCr(l n, l k, l mod) {
+  l a = 1;
+  for (l i = n; i > n - k; i--) a = mult_mod(a, i, mod);
+  l b = 1;
+  F(i, 1, k + 1) b = mult_mod(b, i, mod);
+  b = inverse_mod(b, mod);
+  return mult_mod(a, b, mod);
+}
+
+// precompute all combinations up to (n n)
+vvl combinations(l n, l mod) {
+  vvl c(n + 1, vl(n + 1));
+  F(i, 0, n) {
+    c[i][0] = 1;
+    F(j, 1, i + 1) {
+      c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % mod;
+    }
+  }
+  return c;
+}
+
+const l MOD = e9 + 7;
+
+void solve(istream& cin, ostream& cout) {
+  l n, k;
+  while (cin >> n >> k) {
+    vll points(n);
+    F(i, 0, n) cin >> points[i].first >> points[i].second;
+    ll o; cin >> o.first >> o.second;
+    vd aa;
+    F(i, 0, n) {
+      double a = atan2(points[i].second - o.second, points[i].first - o.first);
+      aa.emplace_back(a);
+      aa.emplace_back(a + 2 * PI);
+    }
+    sort(all(aa));
+    l j = 0;
+    l answer = nCr(n, k, MOD);
+    vl c(n - k + 2); // c[i] = C((i + k - 1), k - 1)
+    c[0] = 1;
+    F(i, 1, n - k + 2) {
+      c[i] = mult_mod(mult_mod(c[i - 1], (i + k - 1), MOD),
+                      inverse_mod(i, MOD),
+                      MOD);
+    }
+    F(i, 0, n) {
+      while ((j + 1) < 2*n and aa[j + 1] < aa[i] + PI + EPS) j++;
+      if (j - i < k - 1) continue;
+      answer = cong(answer - c[j - i - k + 1], MOD);
+    }
+    cout << answer << lf;
+  }
 }
 
 int main() {
